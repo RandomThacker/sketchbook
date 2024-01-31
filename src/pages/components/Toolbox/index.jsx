493861ed3413d367849d8e9a8116e6d6a329e5aa
RandomThacker <randomthacker@gmail.com>
2024-01-31 10:21:15 +0530

    toolbox active feat

--- src/pages/components/Toolbox/index.jsx
+++ src/pages/components/Toolbox/index.jsx
@@ -2,85 +2,45 @@ import { COLORS, MENU_ITEMS } from "@/constants";
 import styles from "./index.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { changeColor, changeBrushSize } from "@/slice/toolboxSlice";
+import cx from 'classnames'
 
 const Toolbox = () => {
   const dispatch = useDispatch();
-
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const showStroke = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrush =
     activeMenuItem === MENU_ITEMS.PENCIL ||
     activeMenuItem === MENU_ITEMS.ERASER;
 
-    const updateBrushSize = (e) => {
+  const {color, size} = useSelector((state) => state.toolbox[activeMenuItem]);
+
+  const updateBrushSize = (e) => {
       dispatch(changeBrushSize({item: activeMenuItem, size: e.target.value}))
     };
 
-    const updateColor = (newColor) => {
+  const updateColor = (newColor) => {
       dispatch(changeColor({item: activeMenuItem, color: newColor}))
     };
 
-
-  return (
-    <div className={styles.toolboxContainer}>
-      {showStroke && (
-        <div className={styles.toolItem}>
-          <h2 className={styles.toolText}>Color</h2>
+    return (<div className={styles.toolboxContainer}>
+      {showStroke && <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.BLACK }}
-              onClick={()=>updateColor(COLORS.BLACK)}
-            />
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.RED }}
-              onClick={()=>updateColor(COLORS.RED)}
-
-            />
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.GREEN }}
-              onClick={()=>updateColor(COLORS.GREEN)}
-            />
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.BLUE }}
-              onClick={()=>updateColor(COLORS.BLUE)}
-
-            />
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.ORANGE }}
-              onClick={()=>updateColor(COLORS.ORANGE)}
-
-            />
-            <div
-              className={styles.colorBox}
-              style={{ backgroundColor: COLORS.YELLOW }}
-              onClick={()=>updateColor(COLORS.YELLOW)}
-
-            />
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLACK})} style={{backgroundColor: COLORS.BLACK}} onClick={() => updateColor(COLORS.BLACK)}/>
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.RED})} style={{backgroundColor: COLORS.RED}} onClick={() => updateColor(COLORS.RED)}/>
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.GREEN})} style={{backgroundColor: COLORS.GREEN}} onClick={() => updateColor(COLORS.GREEN)}/>
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLUE})} style={{backgroundColor: COLORS.BLUE}} onClick={() => updateColor(COLORS.BLUE)}/>
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.ORANGE})} style={{backgroundColor: COLORS.ORANGE}} onClick={() => updateColor(COLORS.ORANGE)}/>
+              <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.YELLOW})} style={{backgroundColor: COLORS.YELLOW}} onClick={() => updateColor(COLORS.YELLOW)}/>
           </div>
-        </div>
-      )}
-
-      {showBrush && (
-        <div className={styles.toolItem}>
-          <h2 className={styles.toolText}>Brush Size</h2>
+      </div>}
+      {showBrush && <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Brush Size</h4>
           <div className={styles.itemContainer}>
-            <input
-              type="range"
-              min={1}
-              max={10}
-              step={1}
-              onChange={updateBrushSize}
-            />
+              <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size}/>
           </div>
-        </div>
-      )}
-    </div>
-  );
-};
+      </div>}
+  </div>)
+}
 
 export default Toolbox;
