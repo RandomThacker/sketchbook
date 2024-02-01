b119fa50de0ffdfd7ee73ebca9da135630134c4a
RandomThacker <randomthacker@gmail.com>
2024-02-01 15:54:15 +0530

    responsive change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Toolbox from "./components/Toolbox";
 
 export default function Home() {
   return (
-    <div className="bg-[#202124]">
+    <div className="bg-[#202124] overflow-hidden sm:h-screen">
       <Menu />
       <Board />
       <Toolbox />
